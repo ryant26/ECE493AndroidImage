e5eafa9c765d470d01d1e4fb7c52c035b4bc05ee
ryant26 <ryant26@gmail.com>
2016-01-08 16:27:40 -0700

    added mean filter settings activity

---
+++ app/src/main/java/ece493/imagemanipulation/MeanFilterActivity.java
@@ -0,0 +1,13 @@
+package ece493.imagemanipulation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeanFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mean_filter);
+    }
+}
