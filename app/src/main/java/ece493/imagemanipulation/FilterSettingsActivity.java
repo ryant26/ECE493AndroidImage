72fbb624fb23052b951a1727fe343f90b7bfb18f
ryant26 <ryant26@gmail.com>
2016-01-08 16:41:58 -0700

    made abstract filter settings activity

--- app/src/main/java/ece493/imagemanipulation/FilterSettingsActivity.java
+++ app/src/main/java/ece493/imagemanipulation/FilterSettingsActivity.java
@@ -3,11 +3,17 @@ package ece493.imagemanipulation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FilterSettingsActivity extends AppCompatActivity {
+public abstract class FilterSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_Settings);
     }
+
+    //Should return the title for the top of the screen
+    protected abstract String getSettingsTitle();
+
+    //Should call the appropiate method for each filter type
+    protected abstract void applyFilter();
 }
