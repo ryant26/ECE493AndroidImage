629284f53615d0748500d2ef4d12856b37289bbb
ryant26 <ryant26@gmail.com>
2016-01-08 16:32:02 -0700

    better OO for filter settings

--- app/src/main/java/ece493/imagemanipulation/FilterSettingsActivity.java
+++ app/src/main/java/ece493/imagemanipulation/FilterSettingsActivity.java
@@ -3,11 +3,11 @@ package ece493.imagemanipulation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MeanFilterActivity extends AppCompatActivity {
+public class FilterSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mean_filter);
+        setContentView(R.layout.activity_filter_Settings);
     }
 }
