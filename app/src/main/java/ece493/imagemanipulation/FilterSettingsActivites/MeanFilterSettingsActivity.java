1fbfaeaa5a8570d3385f20917b8cad8b2847ad81
ryant26 <ryant26@gmail.com>
2016-01-08 17:10:41 -0700

    returnign titles

--- app/src/main/java/ece493/imagemanipulation/FilterSettingsActivites/MeanFilterSettingsActivity.java
+++ app/src/main/java/ece493/imagemanipulation/FilterSettingsActivites/MeanFilterSettingsActivity.java
@@ -1,12 +1,14 @@
 package ece493.imagemanipulation.FilterSettingsActivites;
 
+import ece493.imagemanipulation.R;
+
 /**
  * Created by ryan on 08/01/16.
  */
 public class MeanFilterSettingsActivity extends FilterSettingsActivity{
     @Override
     protected String getSettingsTitle() {
-        return null;
+        return getResources().getString(R.string.mean_filter_settings);
     }
 
     @Override
--- app/src/main/java/ece493/imagemanipulation/FilterSettingsActivites/MedianFilterSettingsActivity.java
+++ app/src/main/java/ece493/imagemanipulation/FilterSettingsActivites/MedianFilterSettingsActivity.java
@@ -1,16 +1,19 @@
 package ece493.imagemanipulation.FilterSettingsActivites;
 
+import ece493.imagemanipulation.R;
+
 /**
  * Created by ryan on 08/01/16.
  */
 public class MedianFilterSettingsActivity extends FilterSettingsActivity {
     @Override
     protected String getSettingsTitle() {
-        return null;
+        return getResources().getString(R.string.median_filter_Settings);
+
     }
 
     @Override
     protected void applyFilter() {
-
+    
     }
 }
