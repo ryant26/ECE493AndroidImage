035ff3bb741502ee2ace3678c5f3cee7230860c8
ryant26 <ryant26@gmail.com>
2016-01-12 18:43:30 -0700

    optimized algorithms further

--- app/src/main/java/ece493/imagemanipulation/Utilities/ImageHelper.java
+++ app/src/main/java/ece493/imagemanipulation/Utilities/ImageHelper.java
@@ -1,11 +1,7 @@
 package ece493.imagemanipulation.Utilities;
 
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
 
 import ece493.imagemanipulation.AppManager;
 
@@ -38,17 +34,21 @@ public class ImageHelper {
 
     private class MeanConvolutionFilter implements ConvolutionFilter{
 
+        int R=0, G=0, B=0;
+
         @Override
         public Integer convolute(int[] mask, int numPixels) {
-            int R=0, G=0, B=0;
+            R=0; G=0; B=0;
             for (int j=0; j < numPixels; j++){
                 R += (mask[j] >> 16) & 0xff;
                 G += (mask[j] >> 8) & 0xff;
                 B += mask[j] & 0xff;
             }
-            R = Math.round((float)R/numPixels);
-            G = Math.round((float)G/numPixels);
-            B = Math.round((float)B/numPixels);
+
+            //Intentionally using integer division dropping the decimal
+            R = R/numPixels;
+            G = G/numPixels;
+            B = B/numPixels;
 
             return 0xff000000 | (R << 16) | (G << 8) | B;
 
@@ -58,11 +58,21 @@ public class ImageHelper {
 
     private class MedianConvolutionFiler implements  ConvolutionFilter{
 
+        int [] RChannels = new int[1];
+        int [] GChannels = new int[1];
+        int [] BChannels = new int[1];
+
+        int R, G, B;
+
         @Override
         public Integer convolute(int[] mask, int numPixels) {
-            int [] RChannels = new int[numPixels];
-            int [] GChannels = new int[numPixels];
-            int [] BChannels = new int[numPixels];
+
+            if (numPixels > RChannels.length){
+                RChannels = new int[numPixels];
+                GChannels = new int[numPixels];
+                BChannels = new int[numPixels];
+            }
+
 
             for (int i=0; i < numPixels; i++){
                 RChannels[i] = (mask[i] >> 16) & 0xff;
@@ -70,25 +80,24 @@ public class ImageHelper {
                 BChannels[i] = mask[i] & 0xff;
             }
 
-            int R = Math.round(computeMedian(RChannels));
-            int G = Math.round(computeMedian(GChannels));
-            int B = Math.round(computeMedian(BChannels));
+            R = computeMedian(RChannels, numPixels);
+            G = computeMedian(GChannels, numPixels);
+            B = computeMedian(BChannels, numPixels);
 
             return 0xff000000 | (R << 16) | (G << 8) | B;
         }
         
-        private float computeMedian(int[] unsortedList){
-            int numElements = unsortedList.length;
+        private int computeMedian(int[] unsortedList, int numElements){
             Arrays.sort(unsortedList, 0, numElements);
-            float median = 0;
+            int median = 0;
 
             if (numElements % 2 == 0){    //Even
-                median = ((float)unsortedList[numElements/2] + unsortedList[numElements/2 - 1])/2;
+                median = (unsortedList[numElements/2] + unsortedList[numElements/2 - 1])/2;
             } else {    //Odd
-                median = (float)unsortedList[numElements/2];
+                median = unsortedList[numElements/2];
             }
 
-            return Math.round(median);
+            return median;
         }
     }
 
